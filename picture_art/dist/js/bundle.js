7a28f26bd193fe8d2938a08f92b47575a8674067
Vladislav <siriusx.info@gmail.com>
2019-05-26 04:26:51 +0600

    Добавил событие открытия окна с подарком при пролистывании до конца

--- picture_art/dist/js/bundle.js
+++ picture_art/dist/js/bundle.js
@@ -139,53 +139,93 @@ module.exports = mainSlider;
 
 let modal = () => {
 	// Модальное окно popup-consultation
-	let popupConsultation = document.querySelector('.popup-consultation');
+	let popupConsultation = document.querySelector('.popup-consultation'),
+	// модальное окно подарка(popup-gift)
+		popupGift = document.querySelector('.popup-gift'),
 	// картинка подарка fixed-gift
-	let giftPic = document.querySelector('.fixed-gift');
+		giftPic = document.querySelector('.fixed-gift'),
+	// Модальное окно popup-design
+		popupDesign = document.querySelector('.popup-design');
+	// 
 
+
+
+	// функция скрывает картинку с подарком
+	let hidenGift = (statusGift) => {
+			giftPic.style.display = statusGift;
+			giftOn =0;
+	};
+
+
+	// модальное окно которое сейчас открыто
+	let overlayOn;
 
   //функция модального окна
-  let bindModal = (btn, overlayStatus, overflowStatus) => {
-		popupConsultation.style.display = overlayStatus;
+  let bindModal = (btn, overlay, overlayStatus, overflowStatus) => {
+		overlayOn = overlay;
+		// console.log(btn);
+		// console.log(overlayOn);
+
+		// вызов нужного модального окна
+		overlay.style.display = overlayStatus;
+		
 		// окно не прокручивается
 		document.body.style.overflow = overflowStatus;
 
 		// если нажали на подарок то:
-		(btn.classList.contains('fixed-gift')) ? hidenGift('none'): '' ;
-		// (btn.classList.contains('fixed-gift')) ? hidenGift('none'): hidenGift('');
-
-		// отключает отсчет времени
+		(overlay.classList.contains('popup-gift')) ? hidenGift('none'): '';
+	
+		// отключает "вызов окна через 60 сек"
 		statusСountdown = 0;
 	};
 	
-
-	// функция скрывает картинку с подарком (или показывает если нужно)
-	let hidenGift = (statusGift) => {
-		giftPic.style.display = statusGift;
-	};
-
 
 
   //событие при клике
   document.body.addEventListener('click', (e) => {
 		let target = e.target;	
+
 		// Должно вызываться модальное окно (класс popup-consultation) [ окно не прокручивается ]
-		(target.classList.contains('button-consultation')) ? bindModal(target, 'block', 'hidden'): '';
+		(target.classList.contains('button-consultation')) ? bindModal(target, popupConsultation, 'block', 'hidden'): '';
+
 		// должно появляться модальное окно (popup-gift)
-		(target.classList.contains('fixed-gift')) ? bindModal(target, 'block', 'hidden'): '';
-		// При нажатии на крестик или подложку - исчезать.
-		(target.classList.contains('popup-close') || target.classList.contains('popup-consultation')) ? bindModal(target, 'none', ''): '';	
+		(target.classList.contains('fixed-gift')) ? bindModal(target, popupGift, 'block', 'hidden'): '';
+		// 
+
+		// Должно вызываться модальное окно (popup-design)
+		(target.classList.contains('button-design')) ? bindModal(target, popupDesign, 'block', 'hidden'): '';
+
+
+		// При нажатии на крестик или подложку открытого модального окна
+		//  - закрывается открытое модальное окно.
+		(target.classList.contains('popup-close') || target == overlayOn) ? bindModal(target, overlayOn, 'none', ''): '';
 	});
 	
 	
-	// модальное окно по истечении 60 секунд
+	// 
 	let statusСountdown = 1;
+	// откроется модальное окно по истечении 60 секунд если ни на что не нажали
 	let countdown = () => {
 		setTimeout(() => {
-			(statusСountdown == 1) ? bindModal(popupConsultation, 'block', 'hidden'): ''
-		}, 60000);
+			(statusСountdown == 1) ? bindModal("", popupConsultation, 'block', 'hidden'): ''
+		}, 6000);
 	};
 	countdown();
+
+
+	// 
+	let giftOn = 1;
+	// Модальное окно при пролистывании до конца страницы
+	window.onscroll = () => {
+		// Высота с учетом прокрутки:
+		let scrollHeight = Math.max(
+			document.body.scrollHeight, document.documentElement.scrollHeight,
+			document.body.offsetHeight, document.documentElement.offsetHeight,
+			document.body.clientHeight, document.documentElement.clientHeight
+		);
+		if (window.scrollY + 1 >= scrollHeight - innerHeight && giftOn == 1) bindModal('', popupGift, 'block', 'hidden');
+	};
+
 
 };
 
